2f7dc24cc8c6b48eb1c0a5db6aad3fd41787703d
lalalala <jamesbt2k3@gmail.com>
2023-02-13 15:10:03 +0700

    loop+init di main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import Engine.Window;
+import org.lwjgl.opengl.GL;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
 //jangan pakai gl30
 
 public class Main {
@@ -12,10 +14,17 @@ public class Main {
     }
 
     public void init(){
-
+        window.init();
+        GL.createCapabilities();
     }
 
     public void loop(){
-
+        while(window.isOpen()){
+            window.update();
+            glClearColor(0.0f,0.0f,0.0f,0.0f);
+            GL.createCapabilities();
+            glDisableVertexAttribArray(0);
+            glfwPollEvents();
+        }
     }
 }
