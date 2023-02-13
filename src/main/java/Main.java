1502b9f40230339e9980dbb8ca62a33d3f30fa55
lalalala <jamesbt2k3@gmail.com>
2023-02-13 15:04:18 +0700

    perubahan kedua

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import Engine.Window;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.glClearColor;
+//jangan pakai gl30
+
+public class Main {
+    private Window window =  new Window(800,800,"Hello World!");
+    public void run(){
+        init();
+        loop();
+    }
+
+    public void init(){
+
+    }
+
+    public void loop(){
+
+    }
+}
